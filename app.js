0cf180e72b3973aa9f8da83d45364853eab657a9
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-04-02 14:35:58 +0300

    Add MongoDB connection and update server configuration

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
-const port = 3000;
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDB = require("./db/mongoDb");
 
+//Connect to MongoDB
+connectDB();
+//Middlewares
+const app = express();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+//Server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
